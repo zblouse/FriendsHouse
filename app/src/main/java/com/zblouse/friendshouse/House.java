393da6dadda94dd26b137f3077d19647a649c475
Zach Blouse <zachblouse@gmail.com>
2024-11-10 12:05:15 -0500

    wip adding basic recycler view

---
+++ app/src/main/java/com/zblouse/friendshouse/House.java
@@ -0,0 +1,46 @@
+package com.zblouse.friendshouse;
+
+import java.io.Serializable;
+
+public class House implements Serializable {
+    private int id;
+    private String name;
+    private String description;
+    private double latitude;
+    private double longitude;
+
+    public House(String name, String description, double latitude, double longitude){
+        this.name = name;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public House(int id, String name, String description, double latitude, double longitude){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public double getLatitude(){
+        return this.latitude;
+    }
+
+    public double getLongitude(){
+        return this.longitude;
+    }
+}
---
+++ app/src/main/java/com/zblouse/friendshouse/HouseDatabaseHelper.java
@@ -0,0 +1,72 @@
+package com.zblouse.friendshouse;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HouseDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "house_database";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "houses";
+    private static final String ID_KEY = "id";
+    private static final String HOUSE_NAME_KEY = "house_name";
+    private static final String HOUSE_DESCRIPTION_KEY = "house_description";
+    private static final String HOUSE_LATITUDE_KEY = "house_latitude";
+    private static final String HOUSE_LONGITUDE_KEY = "house_longitude";
+
+    public HouseDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTableQuery = "CREATE TABLE " + TABLE_NAME + "(" +
+                ID_KEY + " INTEGER PRIMARY KEY," +
+                HOUSE_NAME_KEY + " TEXT," +
+                HOUSE_DESCRIPTION_KEY + " TEXT," +
+                HOUSE_LATITUDE_KEY + " REAL," +
+                HOUSE_LONGITUDE_KEY + " REAL)";
+
+        sqLiteDatabase.execSQL(createTableQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public void addHouseToDatabase(House house){
+        SQLiteDatabase database = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(HOUSE_NAME_KEY, house.getName());
+        contentValues.put(HOUSE_DESCRIPTION_KEY, house.getDescription());
+        contentValues.put(HOUSE_LATITUDE_KEY, house.getLatitude());
+        contentValues.put(HOUSE_LONGITUDE_KEY, house.getLongitude());
+        database.insert(TABLE_NAME,null,contentValues);
+
+        database.close();
+    }
+
+    public List<House> getAllHouses(){
+        SQLiteDatabase database = getReadableDatabase();
+        Cursor houseCursor = database.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        ArrayList<House> houses = new ArrayList<>();
+        if(houseCursor.moveToFirst()){
+            do {
+                House house = new House(houseCursor.getInt(0),houseCursor.getString(1), houseCursor.getString(2), houseCursor.getDouble(3), houseCursor.getDouble(4));
+                houses.add(house);
+            } while(houseCursor.moveToNext());
+        }
+
+        database.close();
+        return houses;
+    }
+}
