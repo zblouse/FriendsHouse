67f312a692dd57957d5b6a8d66abab83ccb006f8
Zach Blouse <zachblouse@gmail.com>
2024-11-10 17:43:38 -0500

    action bar

--- app/src/main/java/com/zblouse/friendshouse/MainActivity.java
+++ app/src/main/java/com/zblouse/friendshouse/MainActivity.java
@@ -1,10 +1,13 @@
 package com.zblouse.friendshouse;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         FloatingActionButton floatingActionButton = findViewById(R.id.floating_button);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +41,22 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, new ListViewFragment()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection.
+        if(item.getItemId() == R.id.action_home){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new ListViewFragment()).commit();
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
